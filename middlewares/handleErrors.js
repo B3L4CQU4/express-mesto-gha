568fb32f357154d05f72c256247165206171d609
Островский Александр <gold1997ask@gmail.com>
2024-02-03 09:56:20 +0300

    hot fix

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -2,7 +2,7 @@ const handleErrors = (error, req, res) => {
   if (error.isJoi) {
     // Ошибка валидации Joi
     const statusCode = 400;
-    const message = error.details.map((detail) => detail.message).join('; ');
+    const { message } = error.message;
     res.status(statusCode).json({ message });
   } else if (error.code === 11000) {
     const statusCode = 409;// Обработка ошибки бд
